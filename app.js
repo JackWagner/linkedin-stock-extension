00efc28ddd64801f99df90b6e4f051af68abac76
Jack Wagner <jack.wagner5299@gmail.com>
2024-12-05 10:43:48 -0600

    hello world with Alpaca SDK

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+require('dotenv').config();
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+
+const key    = process.env.key;
+const secret = process.env.secret;
+
+const alpaca = new Alpaca({
+    keyId: key,
+    secretKey: secret,
+    paper: true,
+  })
+
+alpaca.getAccount().then((account) => {
+    console.log('Current Account:', account)
+})
