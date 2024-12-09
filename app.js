6b46242950fcd39388e82698b67b88a0e05f8146
Jack Wagner <jack.wagner5299@gmail.com>
2024-12-09 10:23:29 -0600

    retrieve historical stock data example

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const alpaca = new Alpaca({
     paper: true,
   })
 
-alpaca.getAccount().then((account) => {
-    console.log('Current Account:', account)
-})
+/* start and end date are YYYY-MM-DD formatted strings */
+async function getBarsV2(ticker, start_date, end_date, ) {
+    const bars = alpaca.getBarsV2(ticker, {
+        start: start_date,
+        end: end_date,
+        timeframe: alpaca.newTimeframe(30, alpaca.timeframeUnit.MIN),
+        limit: 1,
+    });
+    const got = [];
+    for await (let b of bars) {
+        got.push(b);
+    }
+    console.log(got);
+}
+
+getBarsV2("SPY","2022-04-01","2022-04-02")
